e12fc14f5c4270e15342b3d18e5f4c93b41117ac
kavyadayanand <kavya.d.dayanand@gmail.com>
2018-11-26 16:51:16 -0800

    Update visualize.py

--- Mask_RCNN/visualize.py
+++ Mask_RCNN/visualize.py
@@ -109,42 +109,43 @@ def display_instances(image, boxes, masks, class_ids, class_names,
         color = colors[i]
 
         # Bounding box
-        if not np.any(boxes[i]):
-            # Skip this instance. Has no bbox. Likely lost in image cropping.
-            continue
-        y1, x1, y2, x2 = boxes[i]
-        p = patches.Rectangle((x1, y1), x2 - x1, y2 - y1, linewidth=2,
-                              alpha=0.7, linestyle="dashed",
-                              edgecolor=color, facecolor='none')
-        ax.add_patch(p)
+        if class_names[class_id] == 'person':  
+            if not np.any(boxes[i]):
+                # Skip this instance. Has no bbox. Likely lost in image cropping.
+                continue
+                y1, x1, y2, x2 = boxes[i]
+                p = patches.Rectangle((x1, y1), x2 - x1, y2 - y1, linewidth=2,
+                                      alpha=0.7, linestyle="dashed",
+                                      edgecolor=color, facecolor='none')
+                ax.add_patch(p)
 
-        # Label
-        class_id = class_ids[i]
-        score = scores[i] if scores is not None else None
-        label = class_names[class_id]
-        x = random.randint(x1, (x1 + x2) // 2)
-        caption = "{} {:.3f}".format(label, score) if score else label
-        ax.text(x1, y1 + 8, caption,
-                color='w', size=11, backgroundcolor="none")
+                # Label
+                class_id = class_ids[i]
+                score = scores[i] if scores is not None else None
+                label = class_names[class_id]
+                x = random.randint(x1, (x1 + x2) // 2)
+                caption = "{} {:.3f}".format(label, score) if score else label
+                ax.text(x1, y1 + 8, caption,
+                        color='w', size=11, backgroundcolor="none")
 
-        # Mask
-        mask = masks[:, :, i]
-        masked_image = apply_mask(masked_image, mask, color)
+                # Mask
+                mask = masks[:, :, i]
+                masked_image = apply_mask(masked_image, mask, color)
 
-        # Mask Polygon
-        # Pad to ensure proper polygons for masks that touch image edges.
-        padded_mask = np.zeros(
-            (mask.shape[0] + 2, mask.shape[1] + 2), dtype=np.uint8)
-        padded_mask[1:-1, 1:-1] = mask
-        contours = find_contours(padded_mask, 0.5)
-        for verts in contours:
-            # Subtract the padding and flip (y, x) to (x, y)
-            verts = np.fliplr(verts) - 1
-            p = Polygon(verts, facecolor="none", edgecolor=color)
-            ax.add_patch(p)
-        if class_names[class_id] == 'person' :    
-            ax.imshow(masked_image.astype(np.uint8))
-            plt.show()
+                # Mask Polygon
+                # Pad to ensure proper polygons for masks that touch image edges.
+                padded_mask = np.zeros(
+                    (mask.shape[0] + 2, mask.shape[1] + 2), dtype=np.uint8)
+                padded_mask[1:-1, 1:-1] = mask
+                contours = find_contours(padded_mask, 0.5)
+                for verts in contours:
+                    # Subtract the padding and flip (y, x) to (x, y)
+                    verts = np.fliplr(verts) - 1
+                    p = Polygon(verts, facecolor="none", edgecolor=color)
+                    ax.add_patch(p)
+
+                ax.imshow(masked_image.astype(np.uint8))
+                plt.show()
     
 
 def draw_rois(image, rois, refined_rois, mask, class_ids, class_names, limit=10):
