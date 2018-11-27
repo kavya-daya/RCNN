8f9ff25cd2007f5c449eb8eb1a5624f49de8e729
kavyadayanand <kavya.d.dayanand@gmail.com>
2018-11-26 16:23:33 -0800

    Update visualize.py

--- Mask_RCNN/visualize.py
+++ Mask_RCNN/visualize.py
@@ -142,9 +142,9 @@ def display_instances(image, boxes, masks, class_ids, class_names,
             verts = np.fliplr(verts) - 1
             p = Polygon(verts, facecolor="none", edgecolor=color)
             ax.add_patch(p)
-    ax.imshow(masked_image.astype(np.uint8))
-    if class_names[class_id] == 'person' :
-        plt.show()
+        if class_names[class_id] == 'person' :    
+            ax.imshow(masked_image.astype(np.uint8))
+            plt.show()
     
 
 def draw_rois(image, rois, refined_rois, mask, class_ids, class_names, limit=10):
